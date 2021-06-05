2d0354527cba757205a295ae37158ad9d5266c0e
leman <leman@lmswork.com>
2021-06-05 17:21:10 +0800

    fix: inccorect map

--- packages/core/src/createIconset.tsx
+++ packages/core/src/createIconset.tsx
@@ -8,13 +8,13 @@ export function createIconset<IconNames extends string>({
   const Iconset = (props: IconsetBaseProps<IconNames>, ref: any) => {
     const { name, ...restProps } = props;
     const Instance = map[name];
-    Instance.displayName = `${familyName}.${name}`;
     if (!Instance) {
       if (process.env.NODE_ENV === "development") {
         console.warn("Icon %s not found from iconset %s.", name, familyName);
       }
       return null;
     }
+    Instance.displayName = `${familyName}.${name}`;
     return <Instance ref={ref} {...restProps} />;
   };
   Iconset.displayName = familyName;
