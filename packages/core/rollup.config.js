fc5941e59e59d0e62d6674aaf78ba8e29ea1ab16
Leman <leman@lmswork.com>
2022-02-20 13:13:46 +0800

    fix: incorrect import reference

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,10 +1,10 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import sourceMaps from "rollup-plugin-sourcemaps";
-import camelCase from "lodash/camelcase";
 import json from "@rollup/plugin-json";
 import external from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
+import { camelCase } from "lodash";
 
 const pkg = require("./package.json");
 
